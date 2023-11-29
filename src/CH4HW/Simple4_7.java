ad31078f8b377b70cc3a4919b7a533ae67361407
BeenMilk <tom34903525.12g@gmail.com>
2023-11-30 01:44:44 +0800

    HW-CH4

---
+++ src/CH4HW/Simple4_7.java
@@ -0,0 +1,36 @@
+package CH4HW;
+
+public class Simple4_7 {
+	
+	public static void main(String[] args) {
+		
+		Car7 car1;
+		car1 = new Car7();
+		
+		car1.setCar7(1234, 20.5);
+		System.out.println(car1);
+	}
+}
+
+class Car7 {
+	
+	protected int num;
+	protected double gas;
+	
+	public Car7() {
+		num = 0;
+		gas = 0;
+		System.out.println("生產了車子");
+	}
+	
+	public void setCar7(int n, double g) {
+		num = n;
+		gas = g;
+		System.out.println("將車號設為"+ num +"，汽油量設為"+ gas);
+	}
+	
+	public String toString() {
+		String str = "車號:"+ num +"汽油量:"+ gas;
+		return str;
+	}
+}
